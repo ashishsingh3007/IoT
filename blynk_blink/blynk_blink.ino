d3d9eeb545851e8804b28241fb0bf089e2701237
ashishsingh3007 <53784155+ashishsingh3007@users.noreply.github.com>
2020-02-09 22:41:34 +0530

    Add files via upload
    Led blink using blynk app.

---
+++ blynk_blink/blynk_blink.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(D0,OUTPUT);
+  pinMode(D7,OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(D0,1);
+  delay(100);
+ 
+   digitalWrite(D7,1);
+  delay(300);
+  digitalWrite(D7,0);
+  delay(300);
+   digitalWrite(D0,0);
+  delay(100);
+}
